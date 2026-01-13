c85375f8c70ff0c42200a9f8075fe73cdcb8d76e
superlucky24 <ghparc24@gmail.com>
2026-01-13 16:38:41 +0900

    성민 브랜치 병합

---
+++ src/main/java/kr/go/Ydpb/service/AdminServiceImpl.java
@@ -0,0 +1,15 @@
+package kr.go.Ydpb.service;
+
+import kr.go.Ydpb.mapper.AdminMapper;
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AdminServiceImpl implements AdminService{
+    //주입
+
+    //메서드 구현
+}
---
+++ src/test/java/kr/go/Ydpb/YdpbApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.go.Ydpb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YdpbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
